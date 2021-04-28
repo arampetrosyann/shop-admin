35395d56c9663cf2cefc006ec7fce277dea25609
Aspram <aspram.markosyan7@gmail.com>
2021-04-28 16:29:18 +0400

    admin menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Sections from "./containers/Sections";
 import "./styles/App.css";
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Sections />
+    </div>
+  );
 };
 
 export default App;
