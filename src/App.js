ae8b1732347a3f4e6f2e6b31a52ed9bea91886c0
arampetrosyan <0aramp@gmail.com>
2021-04-30 02:17:02 +0400

    create router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
+import Routes from "./routes";
 import "./styles/App.css";
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
