48a2b532f16c9edda833f58d34d54e5910b2f98a
arampetrosyan <0aramp@gmail.com>
2021-04-30 15:55:54 +0400

    create btn component

---
+++ src/components/Button/button.js
@@ -0,0 +1,35 @@
+import React from "react";
+import defaultClasses from "./button.module.css";
+import mergeClasses from "../../helpers/mergeClasses";
+
+const Button = (props) => {
+  const {
+    type,
+    value,
+    style,
+    id,
+    disabled,
+    autoFocus,
+    onClick,
+    children,
+  } = props;
+
+  const classes = mergeClasses(defaultClasses, props.classes);
+
+  return (
+    <button
+      className={classes.root}
+      style={style}
+      type={type}
+      value={value}
+      id={id}
+      onClick={onClick}
+      autoFocus={autoFocus}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
