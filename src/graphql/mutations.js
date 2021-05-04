84a318bb78ffbda199a61e2934ce630aa2d94378
arampetrosyan <0aramp@gmail.com>
2021-05-04 16:00:22 +0400

    add sign in mutation

---
+++ src/graphql/mutations.js
@@ -0,0 +1,11 @@
+import { gql } from "@apollo/client";
+
+export const SIGN_IN = gql`
+  mutation($email: String!, $password: String!) {
+    adminLogin(email: $email, password: $password) {
+      firstname
+      lastname
+      token
+    }
+  }
+`;
