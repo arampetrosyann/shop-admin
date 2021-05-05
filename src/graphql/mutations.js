5617eaef537ec5c1e90c80e2bdba5b7bb6eb14f4
arampetrosyan <0aramp@gmail.com>
2021-05-05 15:17:13 +0400

    create add customer mutation

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -9,3 +9,21 @@ export const SIGN_IN = gql`
     }
   }
 `;
+
+export const ADD_CUSTOMER = gql`
+  mutation(
+    $email: String
+    $password: String
+    $firstname: String
+    $lastname: String
+  ) {
+    adminAddCustomer(
+      customerData: {
+        email: $email
+        password: $password
+        firstname: $firstname
+        lastname: $lastname
+      }
+    )
+  }
+`;
