890335bca07c794626bd0792276633a949a5b556
Aspram <aspram.markosyan7@gmail.com>
2021-05-17 04:07:47 +0400

    update add slider page

--- src/containers/AddSlider/addSlider.js
+++ src/containers/AddSlider/addSlider.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuWrapper from "../../components/MenuWrapper";
 import Form from "../../components/Form";
 import useAddSlider from "../../talons/useAddSlider";
 import classes from "./addSlider.module.css";
 
 const AddSlider = () => {
-  const { fields, formik, disableBtn } = useAddSlider();
+  const [selectToggle, setSelectToggle] = useState(false);
+
+  const change = (e) => {
+    setSelectToggle(!selectToggle);
+    setSelectValue(e.target.value);
+  };
+
+  const { fields, formik } = useAddSlider();
+
   return (
     <MenuWrapper>
       <div className={classes.root}>
-        <h1 className={classes.heading}>Ավելացնել ապրանք</h1>
+        <h1 className={classes.heading}>Ավելացնել սլայդեր</h1>
         <Form
           action="/"
           method="POST"
           btnValue="Ավելացնել"
           fields={fields}
-          disableBtn={disableBtn}
           onSubmit={formik.handleSubmit}
+          classes={{ root: classes.root }}
         />
       </div>
     </MenuWrapper>
--- src/talons/useAddSlider.js
+++ src/talons/useAddSlider.js
@@ -1,64 +1,31 @@
-import { useMemo, useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useMemo } from "react";
 import { useFormik } from "formik";
-import * as Yup from "yup";
-// import { useMutation } from "@apollo/client";
-// import { ADD_SLIDER } from "../graphql/mutations";
-
-const validationSchema = Yup.object().shape({
-  sliderName: Yup.string(),
-  imageName: Yup.string(),
-  image: Yup.mixed(),
-});
 
 const useAddSlider = () => {
-  const [disableBtn, setDisableBtn] = useState(false);
-  //   const [addSlider] = useMutation(ADD_SLIDER);
-  const history = useHistory();
-
   const formik = useFormik({
     initialValues: {
-      sliderName: "",
-      imageName: "",
-      image: null,
+      parent: "",
     },
-    validationSchema,
-    onSubmit: async () => {},
+    onSubmit: (values) => {},
+    enableReinitialize: true,
   });
 
   const fields = useMemo(() => {
     return [
       {
-        field: "input",
-        type: "text",
-        name: "sliderName",
-        id: "sliderName",
-        value: formik.values.sliderName,
-        placeholder: "Սլայդերի անուն",
-        onChange: formik.handleChange,
-      },
-      {
-        field: "input",
-        type: "text",
-        name: "imageName",
-        id: "imageName",
-        value: formik.values.imageName,
-        placeholder: "Նկարի անուն",
-        onChange: formik.handleChange,
-      },
-      {
-        field: "files",
-        placeholder: "Ավելացնել նկար",
-        accepts: ["image/*"],
-        maxFileSize: 10000000,
-        onChange: (files) => {
-          formik.setFieldValue("image", files[0]);
-        },
+        field: "select",
+        label: "Ծնող",
+        options: [
+          { value: "Բաժակ", label: "Բաժակ" },
+          { value: "Գրիչ", label: "Գրիչ" },
+          { value: "Նվեր քարտ", label: "Նվեր քարտ" },
+        ],
+        onChange: (value) => formik.setFieldValue("parent", value),
       },
     ];
   }, [formik]);
 
-  return { fields, formik, disableBtn };
+  return { fields, formik };
 };
 
 export default useAddSlider;
